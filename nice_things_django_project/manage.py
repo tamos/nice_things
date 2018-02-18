41cfc09b81daa73a5bb74f379091fa33733382e2
Alexander Tyan <at1079@georgetown.edu>
2018-02-17 19:05:28 -0600

    Code to update database Food and Wages tables

--- nice_things_django_project/manage.py
+++ nice_things_django_project/manage.py
@@ -1,6 +1,145 @@
 #!/usr/bin/env python
 import os
 import sys
+import pandas as pd
+from dateutil import parser
+import pytz
+
+# To load the Django app, as per https://stackoverflow.com/questions/25537905/
+# django-1-7-throws-django-core-exceptions-
+# appregistrynotready-models-arent-load:
+from django.core.wsgi import get_wsgi_application
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nice_things_django_project.settings'
+application = get_wsgi_application()
+
+from itinerary.models import Flag, Food, Wages
+
+
+def update_wages_table(csv_path="../bls_chicago.csv"):
+    """
+    Updates the Wages table in the nice_things db.
+
+    :param csv_path: string, csv path file
+
+    :return: Update the Wages table in nice_things database
+    """
+    use_cols = ["case_id", "trade_nm", "legal_name", "street_addr_1_txt",
+                "cty_nm", "st_cd", "zip_cd", "case_violtn_cnt"]
+    df = pd.read_csv(filepath_or_buffer=csv_path,
+                     dtype={"case_id": int,
+                            "trade_nm": str,
+                            "legal_name": str,
+                            "street_addr_1_txt": str,
+                            "cty_nm": str,
+                            "st_cd": str,
+                            "zip_cd": str,
+                            "case_violtn_cnt": int},
+                     usecols=use_cols)
+
+    # Clean out nulls to avoid nullable entries into the itinerary_wages table
+    # in the nice_things database:
+    df = df.dropna()
+
+    # Squirt into the database:
+    for index, row in df.iterrows():
+        # Prepare row names:
+        case_id = row["case_id"]
+        trade_nm = row["trade_nm"]
+        legal_name = row["legal_name"]
+        street_addr_1_txt = row["street_addr_1_txt"]
+        cty_nm = row["cty_nm"]
+        st_cd = row["st_cd"]
+        zip_cd = row["zip_cd"]
+        case_violtn_cnt = row["case_violtn_cnt"]
+
+        # Squirt the data into the db:
+        obj, created = Wages.objects.get_or_create(
+            case_id=case_id,
+            trade_nm=trade_nm,
+            legal_name=legal_name,
+            street_addr_1_txt=street_addr_1_txt,
+            cty_nm=cty_nm,
+            st_cd=st_cd,
+            zip_cd=zip_cd,
+            case_violtn_cnt=case_violtn_cnt)
+
+
+def update_food_table(csv_path="../cdp_food_dump.csv"):
+    """
+    Updates the Food table in the nice_things db.
+
+    :param csv_path: string, csv path file
+
+    :return: Update the Food table in nice_things database
+    """
+    use_cols = ["inspection_id", "dba_name", "aka_name", "license_",
+                "facility_type", "risk", "address", "city", "state",
+                "zip", "inspection_date", "inspection_type", "results",
+                "violations", "longitude", "latitude"]
+    df = pd.read_csv(filepath_or_buffer=csv_path,
+                     dtype={"inspection_id": int,
+                            "dba_name": str,
+                            "aka_name": str,
+                            "license_": int,
+                            "facility_type": str,
+                            "risk": str,
+                            "address": str,
+                            "city": str,
+                            "state": str,
+                            "zip": int,
+                            "inspection_date": str,
+                            "inspection_type": str,
+                            "results": str,
+                            "violations": str,
+                            "longitude": float,
+                            "latitude": float},
+                     usecols=use_cols)
+    # Clean out nulls to avoid nullable entries into the itinerary_wages table
+    # in the nice_things database:
+    df = df.dropna()
+
+    # Squirt into the database:
+    for index, row in df.iterrows():
+        # Prepare row names:
+        inspection_id = row["inspection_id"]
+        dba_name = row["dba_name"]
+        aka_name = row["aka_name"]
+        license_num = row["license_"]
+        facility_type = row["facility_type"]
+        risk = row["risk"]
+        address = row["address"]
+        city = row["city"]
+        state = row["state"]
+        zip_ = row["zip"]
+        converted_date = parser.parse(row["inspection_date"])
+        converted_date = converted_date.replace(tzinfo=pytz.UTC)
+        inspection_date = converted_date
+        inspection_type = row["inspection_type"]
+        results = row["results"]
+        violations = row["violations"]
+        longitude = row["longitude"]
+        latitude = row["latitude"]
+
+
+        # Squirt the data in to the db:
+        obj, created = Food.objects.get_or_create(
+            inspection_id=inspection_id,
+            dba_name=dba_name,
+            aka_name=aka_name,
+            license_num=license_num,
+            facility_type=facility_type,
+            risk=risk,
+            address=address,
+            city=city,
+            state=state,
+            zip=zip_,
+            inspection_date=inspection_date,
+            inspection_type=inspection_type,
+            results=results,
+            violations=violations,
+            longitude=longitude,
+            latitude=latitude)
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nice_things_django_project.settings")
@@ -13,3 +152,4 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
--- search.py
+++ search.py
@@ -92,96 +92,96 @@ def match_places(coordinates, places_set):
 # If user input in places set then query the database
 
 LEGAL_DICT_INPUTS = {"inspection_id", "dba_name", "aka_name", "license_",
-					 "facility_type", "risk", "address", "city", "state",
-					 "zip", "inspection_date", "inspection_type", "results",
-					 "violations", "latitude", "longitude", "location",
-					 "location_city","location_address", "location_zip",
-					 "location_state"}
+                     "facility_type", "risk", "address", "city", "state",
+                     "zip", "inspection_date", "inspection_type", "results",
+                     "violations", "latitude", "longitude", "location",
+                     "location_city","location_address", "location_zip",
+                     "location_state"}
 
 
 def pull_cdp_health_api(input_dict, output_csv=None, limit=None,
-						legal_dict_inputs=LEGAL_DICT_INPUTS):
-	"""
-	Connects to the Chicago Data Portal and downloads the Food Inspections
-	data set according to user preferences specified in the function
-	parameters
-	:param input_dict: dictionary of params for what fields from the dataset
-	to include and how to filter them. Dict keys are strings. For full field
-	interpretation see "cdp_food_inspections_description.pdf"
-		Options for keys:
-			inspection_id: number
-			dba_name: text, all capital letters
-			aka_name: text, all capital letters
-			license_: number
-			facility_type: text
-			risk: text
-			address: text, all capital letters
-			city: text, all capital letters
-			state: text
-			zip: number
-			inspection_date: floating_timestamp
-			inspection_type: text
-			results: text
-			violations: text
-			latitude: number
-			longitude: number
-			location: point
-			location_city: text
-			location_address: text
-			location_zip: text
-			location_state: text
-	:param output_csv: string, write out a csv file with this string as a name.
-					   WILL OVERWRITE EXISTING FILE IF NAME MATCHES!
-	:param limit: integer, limit how many most recent rows. API returns 1000 by
-				  default.
-	:param legal_dict_inputs: possible keys for the input_dict
-	:return: pandas filtered dataframe.
-	"""
-	# Check input_dict is a dictionary:
-	if not isinstance(input_dict, dict):
-		raise AssertionError("Not a dictionary in input_dict input!")
+                        legal_dict_inputs=LEGAL_DICT_INPUTS):
+    """
+    Connects to the Chicago Data Portal and downloads the Food Inspections
+    data set according to user preferences specified in the function
+    parameters
+    :param input_dict: dictionary of params for what fields from the dataset
+    to include and how to filter them. Dict keys are strings. For full field
+    interpretation see "cdp_food_inspections_description.pdf"
+        Options for keys:
+            inspection_id: number
+            dba_name: text, all capital letters
+            aka_name: text, all capital letters
+            license_: number
+            facility_type: text
+            risk: text
+            address: text, all capital letters
+            city: text, all capital letters
+            state: text
+            zip: number
+            inspection_date: floating_timestamp
+            inspection_type: text
+            results: text
+            violations: text
+            latitude: number
+            longitude: number
+            location: point
+            location_city: text
+            location_address: text
+            location_zip: text
+            location_state: text
+    :param output_csv: string, write out a csv file with this string as a name.
+                       WILL OVERWRITE EXISTING FILE IF NAME MATCHES!
+    :param limit: integer, limit how many most recent rows. API returns 1000 by
+                  default.
+    :param legal_dict_inputs: possible keys for the input_dict
+    :return: pandas filtered dataframe.
+    """
+    # Check input_dict is a dictionary:
+    if not isinstance(input_dict, dict):
+        raise AssertionError("Not a dictionary in input_dict input!")
 
-	# Check output_csv is a string:
-	if output_csv and not isinstance(output_csv, str):
-		raise AssertionError("Not a string in output_csv input!")
+    # Check output_csv is a string:
+    if output_csv and not isinstance(output_csv, str):
+        raise AssertionError("Not a string in output_csv input!")
 
-	# Concatenate URL:
-	source_url = "https://data.cityofchicago.org/resource/cwig-ma7x.csv"
-	# Make sure to get the latest inspection rows by date:
-	concatenate_url = source_url + "?$order=inspection_date DESC"
-	if limit:  # If want to limit how many rows
-		concatenate_url += "&$limit=" + str(limit)
+    # Concatenate URL:
+    source_url = "https://data.cityofchicago.org/resource/cwig-ma7x.csv"
+    # Make sure to get the latest inspection rows by date:
+    concatenate_url = source_url + "?$order=inspection_date DESC"
+    if limit:  # If want to limit how many rows
+        concatenate_url += "&$limit=" + str(limit)
 
-	for key, value in input_dict.items():
-		# Foolproof key inputs:
-		if key not in legal_dict_inputs:
-			raise AssertionError("Not a valid key provided in the input dictionary!")
-		api_string_to_add = "&{}={}".format(key, value)
-		concatenate_url += api_string_to_add
+    for key, value in input_dict.items():
+        # Foolproof key inputs:
+        if key not in legal_dict_inputs:
+            raise AssertionError("Not a valid key provided in the input dictionary!")
+        api_string_to_add = "&{}={}".format(key, value)
+        concatenate_url += api_string_to_add
 
-	# Get into API (with app tokens, there's no throttling limit):
-	app_token = cdp_tokens.APP_TOKEN
-	socrata_headers = {'X-App-Token': app_token}
-	r = requests.get(url=concatenate_url, headers=socrata_headers)
+    # Get into API (with app tokens, there's no throttling limit):
+    app_token = cdp_tokens.APP_TOKEN
+    socrata_headers = {'X-App-Token': app_token}
+    r = requests.get(url=concatenate_url, headers=socrata_headers)
 
-	# Process the data:
-	csv_data = r.text
-	df = pd.read_csv(io.StringIO(csv_data), index_col="inspection_id")
+    # Process the data:
+    csv_data = r.text
+    df = pd.read_csv(io.StringIO(csv_data), index_col="inspection_id")
 
-	# Dump into csv, if necessary:
-	if output_csv:
-		df.to_csv(output_csv)
+    # Dump into csv, if necessary:
+    if output_csv:
+        df.to_csv(output_csv)
 
-	return df
+    return df
 
 # Using "place" as a parameter query flags table. Then go to health, labour,
 # environmental, etc. tables and query those tables. 
-	# flags JOIN health -> returns healthcode data
-	# flags JOIN labour -> returns labour data
-	# flags JOIN environment -> returns env data
-		# put returned flags data into a queue and have a function match
-		# the flags with businesses
-		
+    # flags JOIN health -> returns healthcode data
+    # flags JOIN labour -> returns labour data
+    # flags JOIN environment -> returns env data
+        # put returned flags data into a queue and have a function match
+        # the flags with businesses
+
 
 ##### STEP 4 #####
 # Query Yelp based on user input
