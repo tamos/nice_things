250c1de2e0a78e24ec75259cec136a5e7e7ee6f7
tamos <tyler.amos@alumni.carleton.ca>
2018-03-05 15:58:09 -0600

    some comments and cleanup on views

--- nice_things_django_project/itinerary/views.py
+++ nice_things_django_project/itinerary/views.py
@@ -7,7 +7,7 @@ from itinerary.forms import ItineraryInputsForm
 # django-1-7-throws-django-core-exceptions-
 # appregistrynotready-models-arent-load:
 import os
-
+import json
 nice_things_project_dir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, nice_things_project_dir)
 from django.core.wsgi import get_wsgi_application
@@ -21,9 +21,16 @@ os.chdir('helpers')
 import helpers.matching as matching   # has to be a better way to do this
 os.chdir( '../')
 
-from django.core import serializers
 
 def index(request):
+    """ This function is the main view. It either takes users' inputs
+    or renders a map with the results.
+    Inputs:
+        - request: the Django request object
+
+    Outputs:
+        - redirects users to either the main page (index.html) or a map (map.html)
+    """
     context = {}
     # Process user inputs:
     if request.method == 'GET':
@@ -47,28 +54,39 @@ def index(request):
             args["categories"] = form.cleaned_data["categories"]
             args["attributes"] = form.cleaned_data["attributes"]
             args["sort"] = form.cleaned_data["sort_by"]
-            # Go get results
-            results = matching.final_result(args)  # search criterion
-            # consider accounting for no results corner case
-            output = point_content(results)# place the info we want into a dict
-            return render(request, 'map.html', {'output':output}) # render the map       
             
+            # Go get results, render as json and output
+            results = matching.final_result(args) 
+            if results.shape[0] > 0: 
+                output = point_content(results)  # place the info we want into json
+                return render(request, 'map.html', {'output':output}) # render the map       
+            # Return to main page if we get no results
+            else:
+                form = ItineraryInputsForm() 
     else:
-        form = ItineraryInputsForm()
+        form = ItineraryInputsForm()  
         
     context["form"] = form
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', context)  # Render main page
 
-import json
 
 def point_content(results):
-    """ This function takes a queryset result and places it into a dictionary
-    for use in the map page.
+    """ This function takes the DataFrame of matched results and
+    places them into a list in the form (latitude, longitude, content)
+    which is then used to generate the points on a map.
+
+    Inputs:
+        - results (DataFrame): object where each row is a single business
+    Outputs:
+        - output (json): a json object which is passed to render in
+        a dictionary to be made into a Javascript array in map.html
     """
     output = []
     for i in results.itertuples():
+        # This is where we insert the marker content
         output.append([i.latitude, i.longitude, i.name])
-    output = mark_safe(json.dumps(output))
+        output = mark_safe(json.dumps(output)) # make sure Django doesn't block it
+
     # References for json/marking safe:
     # https://stackoverflow.com/questions/4698220/django-template-convert-a-python-list-into-a-javascript-object
     # https://stackoverflow.com/questions/739942/how-to-pass-an-array-in-django-to-a-template-and-use-it-with-javascript#739974
